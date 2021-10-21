1c34a50e12495a8e167b9460e8e2da9ed6be2f82
Alexander de Sousa <alexander.jose.de.sousa@gmail.com>
2021-10-21 14:10:04 +0200

    Simplified the decimal macro

--- lib/decimal_env.ex
+++ lib/decimal_env.ex
@@ -1,375 +1,136 @@
 defmodule DecimalEnv do
   @moduledoc """
-  This module provides two macros to be able to use `Decimal`s with regular
-  Elixir operators i.e.
-
-      iex> import DecimalEnv
-      iex> decimal do
-      ...>   21.0 + "21.0"
-      ...> end
-      #Decimal<42.0>
-
-  It is possible to provide a decimal context as argument for the macro i.e
-
-      iex> import DecimalEnv
-      iex> decimal context: [precision: 2] do
-      ...>   1 / 3
-      ...> end
-      #Decimal<0.33>
-
-  > For more information about context check `Decimal.Context` struct in
-  > `Decimal` repository.
-
-  Not all the Elixir statements are valid. The purpose of these macros is to
-  make it easier to write formulas involving `Decimal` type, so writing Elixir
-  code inside the `decimal` block is discouraged i.e:
-
-      iex> import DecimalEnv
-      iex> Enum.reduce([10.99, 10.01, 20.99, 0.01], 0.00,
-      ...>   fn x, acc ->
-      ...>     decimal do: x + acc
-      ...>   end)
-      #Decimal<42.00>
-
-  The previous example calculates the sum of a list of `Float`s, but returns
-  the result as a `Decimal` type. The code is more clear than its equivalent
-  without the macro:
-
-      iex> Enum.reduce([10.99, 10.01, 20.99, 0.01], Decimal.new(0.00),
-      ...>   fn x, %Decimal{} = acc ->
-      ...>     Decimal.add(Decimal.new(x), acc)
-      ...>   end)
-      #Decimal<42.00>
-
-  The only statement offered is `if` statement i.e:
-
-      iex> import DecimalEnv
-      iex> a = 42.0
-      iex> decimal do
-      ...>   if a > 10, do: a, else: 10
-      ...> end
-      #Decimal<42.0>
-
-  > The guard in the `if` statement should be a decimal comparison.
-
-  It is posible to assign variables inside the block, but pattern matching is
-  not available i.e:
-
-      iex> import DecimalEnv
-      iex> decimal context: [precision: 2] do
-      ...>   a = div(42.1, 2.0)
-      ...>   a * 2
-      ...> end
-      #Decimal<42>
-
-  > Constant values inside the decimal block are precalculated at compile time.
-
-  To avoid calculating the `Decimal` value every time a variable external to
-  the block is mentioned inside the block, use the `:bind` i.e:
-
-      iex> import DecimalEnv
-      iex> a = 3
-      iex> x = 4
-      iex> decimal bind: [a: a] do
-      ...>   a * (x + 1 + a * a)
-      ...> end
-      #Decimal<42>
-
-  In the previous example, the variable `a` is converted to `Decimal` only one
-  time instead of three times. The variable `x` only appears one time, so there
-  is no need to add it to the bind `Keyword` list.
-
-  Additionally you can change the name of the external variable by changing the
-  name of the key associated with it i.e:
-
-      iex> import DecimalEnv
-      iex> a = 3
-      iex> x = 4
-      iex> decimal bind: [b: a] do
-      ...>   b * (x + 1 + b * b)
-      ...> end
-      #Decimal<42>
-
-  In the previous example, the external variable `a` is renamed to `b` inside
-  the decimal block.
+  This module provides macros to encapsulate `Decimal` operations while using
+  Elixir arithmetic and comparison operators and functions.
   """
+
+  @typedoc """
+  Type name.
+  """
+  @type type_name ::
+          nil
+          | :decimal
+          | :float
+          | :integer
+          | :string
+          | :scientific
+          | :xsd
+          | :raw
+
+  @typedoc """
+  Context.
+  """
+  @type context ::
+          nil
+          | keyword()
+          | Decimal.Context.t()
+
+  @typedoc """
+  Output type.
+  """
+  @type output ::
+          integer()
+          | float()
+          | binary()
+          | Decimal.t()
+
+  @typedoc """
+  Option.
+  """
+  @type option ::
+          {:as, type_name()}
+          | {:context, context()}
+
+  @typedoc """
+  Options.
+  """
+  @type options :: [option()]
 
   @doc """
-  This macro receives a list of `options` and a do `block` (`list` argument).
-  The `options` available are:
-  
-    * `context`: Keyword list containing the context definition. The keys have
-    the same name as the ones in the `Decimal.Context` struct.
-    * `bind`: Keyword list that indicates which external variables should be
-    converted to `Decimal` right away before the block is executed.
+  Runs a block under the `Decimal` environment. All numeric values will be
+  converted to `Decimal`.
 
-  ## Example
+  Options:
+    - `:context` - `Decimal.Context` struct. it can also be a `keyword` list.
+    - `:as` - In which type we should have the result. The possible values are
+      the following:
+      + `:decimal` (default)
+      + `:float`
+      + `:integer`
+      + `:string`
+      + `:xsd`
+      + `:raw`
+      + `:scientific`
+
+  ## Examples
 
       iex> import DecimalEnv
-      iex> decimal context: [precision: 2] do
-      ...>   21.0 * 2
+      iex> decimal do: 42.42
+      #Decimal<42.42>
+      iex> decimal do: 84.0 - 21 - "21"
+      #Decimal<42.0>
+      iex> decimal as: :scientific do
+      ...>   "0.0000000002" + 0.000000004
       ...> end
-      #Decimal<42>
+      "4.2E-9"
+      iex> decimal context: [precision: 2, rounding: :ceiling], as: :integer do
+      ...>   21.1 + 20
+      ...> end
+      42
   """
-  defmacro decimal(options, do: block) do
-    bound = bind_decimal(options[:bind])
-    context = generate_context(options[:context])
-    block =
-      case expand(block) do
-        {:__block__, _, stmts} ->
-          {:__block__, [], bound ++ stmts}
-        other ->
-          {:__block__, [], bound ++ [other]}
-      end
+  @spec decimal(Macro.t()) :: Macro.t()
+  @spec decimal(options(), Macro.t()) :: Macro.t()
+  defmacro decimal(options \\ [], do: block) do
     quote do
-      Decimal.with_context(unquote(context), fn ->
-        unquote(block)
-      end)
+      Decimal.Context.with(
+        DecimalEnv.get_context(unquote(options[:context])),
+        fn ->
+          use DecimalEnv.Operators
+
+          result = DecimalEnv.Operators.+(unquote(block))
+
+          DecimalEnv.convert_type(result, unquote(options[:as]))
+        end
+      )
     end
   end
 
-  @doc """
-  This macro receives a decimal do `block` (`list` argument). The global
-  `Decimal` context is used to do the operations inside the `block`.
-
-  ## Example
-
-      iex> import DecimalEnv
-      iex> decimal do: 21.0 * 2
-      #Decimal<42.0>
-  """
-  defmacro decimal(do: block) do
-    block = expand(block)
-    quote do: (fn -> unquote(block) end).()
-  end
-
-  ##
-  # Expands the code to include the Decimal funcions instead.
-  defp expand(str) when is_binary(str) do
-    str |> parse() |> precalculated()
-  end
-  defp expand(number) when is_number(number) do
-    number |> Decimal.new() |> precalculated()
-  end
-  defp expand(atom) when is_atom(atom) do
-    atom
-  end
-  defp expand({:__block__, context, stmts}) do
-    stmts = Enum.map(stmts, &expand/1)
-    quote do: unquote({:__block__, context, stmts})
-  end
-  defp expand({:+, _, [num]}) do
-    num = expand(num)
-    quote do: Decimal.plus(unquote(num))
-  end
-  defp expand({:+, _, [lhs, rhs]}) do
-    lhs = expand(lhs)
-    rhs = expand(rhs)
-    quote do: Decimal.add(unquote(lhs), unquote(rhs))
-  end
-  defp expand({:-, _, [num]}) do
-    num = expand(num)
-    quote do: Decimal.minus(unquote(num))
-  end
-  defp expand({:-, _, [lhs, rhs]}) do
-    lhs = expand(lhs)
-    rhs = expand(rhs)
-    quote do: Decimal.sub(unquote(lhs), unquote(rhs))
-  end
-  defp expand({:*, _, [lhs, rhs]}) do
-    lhs = expand(lhs)
-    rhs = expand(rhs)
-    quote do: Decimal.mult(unquote(lhs), unquote(rhs))
-  end
-  defp expand({:/, _, [lhs, rhs]}) do
-    lhs = expand(lhs)
-    rhs = expand(rhs)
-    quote do: Decimal.div(unquote(lhs), unquote(rhs))
-  end
-  defp expand({:>, _, [lhs, rhs]}) do
-    lhs = expand(lhs)
-    rhs = expand(rhs)
-    quote do: Decimal.cmp(unquote(lhs), unquote(rhs)) == :gt
-  end
-  defp expand({:<, context, [lhs, rhs]}) do
-    expand({:>, context, [rhs, lhs]})
-  end
-  defp expand({:==, _, [lhs, rhs]}) do
-    lhs = expand(lhs)
-    rhs = expand(rhs)
-    quote do: Decimal.cmp(unquote(lhs), unquote(rhs)) == :eq
-  end
-  defp expand({:>=, context, args}) do
-    gt = expand({:>, context, args})
-    eq = expand({:==, context, args})
-    quote do: unquote(gt) or unquote(eq)
-  end
-  defp expand({:<=, context, args}) do
-    lt = expand({:<, context, args})
-    eq = expand({:==, context, args})
-    quote do: unquote(lt) or unquote(eq)
-  end
-  defp expand({:!=, context, args}) do
-    eq = expand({:==, context, args})
-    quote do: not unquote(eq)
-  end
-  defp expand({:abs, _, [num]}) do
-    num = expand(num)
-    quote do: Decimal.abs(unquote(num))
-  end
-  defp expand({:inf?, _, [num]}) do
-    num = expand(num)
-    quote do: Decimal.inf?(unquote(num))
-  end
-  defp expand({:max, _, [lhs, rhs]}) do
-    lhs = expand(lhs)
-    rhs = expand(rhs)
-    quote do: Decimal.max(unquote(lhs), unquote(rhs))
-  end
-  defp expand({:min, _, [lhs, rhs]}) do
-    lhs = expand(lhs)
-    rhs = expand(rhs)
-    quote do: Decimal.min(unquote(lhs), unquote(rhs))
-  end
-  defp expand({:nan?, _, [num]}) do
-    num = expand(num)
-    quote do: Decimal.nan?(unquote(num))
-  end
-  defp expand({:div, _, [lhs, rhs]}) do
-    lhs = expand(lhs)
-    rhs = expand(rhs)
-    quote do: Decimal.div_int(unquote(lhs), unquote(rhs))
-  end
-  defp expand({:reduce, _, [num]}) do
-    num = expand(num)
-    quote do: Decimal.reduce(unquote(num))
-  end
-  defp expand({:rem, _, [lhs, rhs]}) do
-    lhs = expand(lhs)
-    rhs = expand(rhs)
-    quote do: Decimal.rem(unquote(lhs), unquote(rhs))
-  end
-  defp expand({:round, _, [num | rest]}) do
-    num = expand(num)
-    quote do: apply(Decimal, :round, [unquote(num) | unquote(rest)])
-  end
-  defp expand({:{}, context, members}) do
-    members = Enum.map(members, &expand/1)
-    quote do: unquote({:{}, context, members})
-  end
-  defp expand(xs) when is_list(xs) do
-    xs = Enum.map(xs, &expand/1)
-    quote do: unquote(xs)
-  end
-  defp expand({fst, snd}) do
-    fst = expand(fst)
-    snd = expand(snd)
-    quote do: {unquote(fst), unquote(snd)}
-  end
-  defp expand({:=, context, [lhs, rhs]}) do
-    lhs = expand_lhs(lhs)
-    rhs = expand(rhs)
-    bind = {:=, context, [lhs, rhs]}
-    quote do: unquote(bind)
-  end
-  defp expand({:if, context, [guard, [do: is_true, else: is_false]]}) do
-    guard = expand(guard)
-    is_true = expand(is_true)
-    is_false = expand(is_false)
-    if_stmt = {:if, context, [guard, [do: is_true, else: is_false]]}
-    quote do: unquote(if_stmt)
-  end
-  defp expand({:and, context, [lhs, rhs]}) do
-    lhs = expand(lhs)
-    rhs = expand(rhs)
-    and_op = {:and, context, [lhs, rhs]}
-    quote do: unquote(and_op)
-  end
-  defp expand({:or, context, [lhs, rhs]}) do
-    lhs = expand(lhs)
-    rhs = expand(rhs)
-    or_op = {:or, context, [lhs, rhs]}
-    quote do: unquote(or_op)
-  end
-  defp expand({name, _, _} = var) when is_atom(name) or is_tuple(name) do
-    quote do: apply(DecimalEnv, :expand_rt, [unquote(var)])
-  end
-
-  ##
-  # Expands a variable on the left. Pattern matching is not allowed inside the
-  # block.
-  defp expand_lhs({name, _, _} = var) when is_atom(name), do: var
+  #########
+  # Helpers
 
   @doc false
-  # Expands variables at runtime.
-  def expand_rt(var) when is_binary(var), do: var |> parse
-  def expand_rt(var) when is_number(var), do: var |> Decimal.new()
-  def expand_rt(var) when is_tuple(var) do
-    var
-    |> Tuple.to_list()
-    |> Enum.map(&expand_rt/1)
-    |> List.to_tuple()
-  end
-  def expand_rt(var) when is_list(var) do
-    var
-    |> Enum.map(&expand_rt/1)
-  end
-  def expand_rt(var), do: var
+  @spec get_context(nil | keyword() | Decimal.Context.t()) ::
+          Decimal.Context.t()
+  def get_context(context)
 
-  ##
-  # Parses a string to Decimal.
-  defp parse(str) when is_binary(str) do
-    case Decimal.parse(str) do
-      :error -> str
-      {:ok, parsed} -> parsed
-    end
+  def get_context(nil) do
+    Decimal.Context.get()
   end
 
-  ##
-  # Returns a Decimal calculated at compile time.
-  defp precalculated(%Decimal{coef: coef, exp: exp, sign: sign}) do
-    quote do
-      %Decimal{coef: unquote(coef), exp: unquote(exp), sign: unquote(sign)}
-    end
-  end
-  defp precalculated(other), do: other
+  def get_context(options) when is_list(options) do
+    options =
+      Decimal.Context.get()
+      |> Map.from_struct()
+      |> Map.to_list()
+      |> Keyword.merge(options)
 
-  ##
-  # Generates context depending on a list of options.
-  defp generate_context(nil) do
-    generate_context([])
-  end
-  defp generate_context([]) do
-    quote do
-      Decimal.get_context()
-    end
-  end
-  defp generate_context(context) do
-    context =
-      Decimal.get_context()
-       |> Map.from_struct()
-       |> Enum.map(fn {key, value} ->
-            {key, Keyword.get(context, key, value)}
-          end)
-    quote do
-      %Decimal.Context{
-        flags: unquote(context[:flags]),
-        precision: unquote(context[:precision]),
-        rounding: unquote(context[:rounding]),
-        traps: unquote(context[:traps])
-      }
-    end
+    struct(Decimal.Context, options)
   end
 
-  ##
-  # Expands variables bound to the block.
-  defp bind_decimal(nil) do
-    []
+  def get_context(%Decimal.Context{} = context) do
+    context
   end
-  defp bind_decimal(list) when is_list(list) do
-    Enum.map(list, fn {k, v} ->
-      name = Macro.var(k, nil)
-      quote do: unquote(name) = DecimalEnv.expand_rt(unquote(v))
-    end)
-  end
+
+  @doc false
+  @spec convert_type(Decimal.t(), type_name()) :: output()
+  def convert_type(decimal, type)
+
+  def convert_type(decimal, :decimal), do: decimal
+  def convert_type(decimal, :float), do: Decimal.to_float(decimal)
+  def convert_type(decimal, :integer), do: Decimal.to_integer(decimal)
+  def convert_type(decimal, :string), do: Decimal.to_string(decimal, :normal)
+  def convert_type(decimal, :scientific), do: Decimal.to_string(decimal)
+  def convert_type(decimal, :xsd), do: Decimal.to_string(decimal, :xsd)
+  def convert_type(decimal, :raw), do: Decimal.to_string(decimal, :raw)
+  def convert_type(decimal, _), do: decimal
 end
--- test/decimal_env_test.exs
+++ test/decimal_env_test.exs
@@ -1,368 +1,119 @@
 defmodule DecimalEnvTest do
   use ExUnit.Case, async: true
-
   doctest DecimalEnv
+  import DecimalEnv
 
-  require DecimalEnv
+  describe "decimal/1" do
+    test "converts integer" do
+      expected = Decimal.new("42")
 
-  test "Expand numeric string" do
-    result =
-      DecimalEnv.decimal do
-        "42.24"
-      end
-    expected = Decimal.new("42.24")
-    assert expected == result
+      result = decimal(do: 42)
+
+      assert Decimal.eq?(expected, result)
+    end
+
+    test "converts float" do
+      expected = Decimal.new("42")
+
+      result = decimal(do: 42.0)
+
+      assert Decimal.eq?(expected, result)
+    end
+
+    test "converts string" do
+      expected = Decimal.new("42")
+
+      result = decimal(do: "42")
+
+      assert Decimal.eq?(expected, result)
+    end
+
+    test "converts variable" do
+      expected = Decimal.new("42")
+      variable = "42.0"
+
+      result = decimal(do: variable)
+
+      assert Decimal.eq?(expected, result)
+    end
+
+    test "supports complex expression" do
+      expected = Decimal.new("42")
+
+      a = 1
+      b = 2
+
+      result =
+        decimal do
+          ("1" - a / b) * 100 + "-8"
+        end
+
+      assert Decimal.eq?(expected, result)
+    end
   end
 
-  test "Expand non-numeric string" do
-    result =
-      DecimalEnv.decimal do
-        "don't panic!"
-      end
-    assert "don't panic!" == result
-  end
+  describe "decimal/2" do
+    test "converts to integer" do
+      result =
+        decimal as: :integer do
+          42
+        end
 
-  test "Expand number" do
-    result =
-      DecimalEnv.decimal do
-        42.24
-      end
-    expected = Decimal.new(42.24)
-    assert expected == result
-  end
+      assert 42 = result
+    end
 
-  test "Expand atom" do
-    result =
-      DecimalEnv.decimal do
-        :atom
-      end
-    assert :atom == result
-  end
+    test "converts to float" do
+      result =
+        decimal as: :float do
+          42
+        end
 
-  test "Expand unary plus" do
-    result =
-      DecimalEnv.decimal do
-        +42.24
-      end
-    expected = 42.24 |> Decimal.new() |> Decimal.plus()
-    assert expected == result
-  end
+      assert 42.0 = result
+    end
 
-  test "Expand binary addition" do
-    result =
-      DecimalEnv.decimal do
-        42.0 + 0.24
-      end
-    expected = Decimal.add(Decimal.new(42.0), Decimal.new(0.24))
-    assert expected == result
-  end
+    test "converts to string" do
+      result =
+        decimal as: :string do
+          -0.00000000042
+        end
 
-  test "Expand unary minus" do
-    result =
-      DecimalEnv.decimal do
-        -42.24
-      end
-    expected = 42.24 |> Decimal.new() |> Decimal.minus()
-    assert expected == result
-  end
+      assert "-0.00000000042" = result
+    end
 
-  test "Expand binary substraction" do
-    result =
-      DecimalEnv.decimal do
-        42.0 - 0.24
-      end
-    expected = Decimal.sub(Decimal.new(42.0), Decimal.new(0.24))
-    assert expected == result
-  end
+    test "converts to scientific" do
+      result =
+        decimal as: :scientific do
+          "0.0000000042"
+        end
 
-  test "Expand binary multiplication" do
-    result =
-      DecimalEnv.decimal do
-        21.0 * 2.0
-      end
-    expected = Decimal.mult(Decimal.new(21.0), Decimal.new(2.0))
-    assert expected == result
-  end
+      assert "4.2E-9" = result
+    end
 
-  test "Expand binary division" do
-    result =
-      DecimalEnv.decimal do
-        43.0 / 2.0
-      end
-    expected = Decimal.div(Decimal.new(43.0), Decimal.new(2.0))
-    assert expected == result
-  end
+    test "converts to xsd format" do
+      result =
+        decimal as: :xsd do
+          "4.2E-9"
+        end
 
-  test "Expand bind operator and block" do
-    result =
-      DecimalEnv.decimal do
-        c = 21.0 * 2
-        c / 2.0
-      end
-    expected =
-      Decimal.div(
-        Decimal.mult(Decimal.new(21.0), Decimal.new(2)), Decimal.new(2.0))
-    assert expected == result
-  end
+      assert "0.0000000042" = result
+    end
 
-  test "Expand greater than" do
-    result =
-      DecimalEnv.decimal do
-        42.0 > 41.0
-      end
-    expected = Decimal.cmp(Decimal.new(42.0), Decimal.new(41.0)) == :gt
-    assert expected == result
-  end
+    test "converts to raw format" do
+      result =
+        decimal as: :raw do
+          "4.2E-9"
+        end
 
-  test "Expand less than" do
-    result =
-      DecimalEnv.decimal do
-        41.0 < 42.0
-      end
-    expected = Decimal.cmp(Decimal.new(41.0), Decimal.new(42.0)) == :lt
-    assert expected == result
-  end
+      assert "42E-10" = result
+    end
 
-  test "Expand equals to" do
-    result =
-      DecimalEnv.decimal do
-        42.0 == 42.0
-      end
-    expected = Decimal.cmp(Decimal.new(42.0), Decimal.new(42.0)) == :eq
-    assert expected == result
-  end
+    test "modifies the context" do
+      result =
+        decimal context: [precision: 2, rounding: :ceiling], as: :string do
+          21.1 + 20
+        end
 
-  test "Expand greater than or equals to" do
-    result =
-      DecimalEnv.decimal do
-        42.1 >= 42.0
-      end
-    expected =
-      Decimal.cmp(Decimal.new(42.1), Decimal.new(42.0)) == :gt or
-      Decimal.cmp(Decimal.new(42.1), Decimal.new(42.0)) == :eq  
-    assert expected == result
-  end
-
-  test "Expand less than or equals to" do
-    result =
-      DecimalEnv.decimal do
-        42.0 <= 42.1
-      end
-    expected =
-      Decimal.cmp(Decimal.new(42.0), Decimal.new(42.1)) == :lt or
-      Decimal.cmp(Decimal.new(42.0), Decimal.new(42.1)) == :eq  
-    assert expected == result
-  end
-
-  test "Expand different to" do
-    result =
-      DecimalEnv.decimal do
-        42.1 != 42.0
-      end
-    expected = Decimal.cmp(Decimal.new(42.1), Decimal.new(42.0)) != :eq
-    assert expected == result
-  end
-
-  test "Expand abs" do
-    result =
-      DecimalEnv.decimal do
-        abs(-42.24)
-      end
-    expected = 42.24 |> Decimal.new() |> Decimal.minus() |> Decimal.abs()
-    assert expected == result
-  end
-
-  test "Expand inf?" do
-    result =
-      DecimalEnv.decimal do
-        inf?("inf")
-      end
-    expected = "inf" |> Decimal.new() |> Decimal.inf?()
-    assert expected == result
-  end
-
-  test "Expand max" do
-    result =
-      DecimalEnv.decimal do
-        max(42.1, 42.0)
-      end
-    expected = Decimal.max(Decimal.new(42.1), Decimal.new(42.0))
-    assert expected == result
-  end
-
-  test "Expand min" do
-    result =
-      DecimalEnv.decimal do
-        min(42.1, 42.0)
-      end
-    expected = Decimal.min(Decimal.new(42.1), Decimal.new(42.0))
-    assert expected == result
-  end
-
-  test "Expand nan?" do
-    result =
-      DecimalEnv.decimal do
-        nan?("nan")
-      end
-    expected = "nan" |> Decimal.new() |> Decimal.nan?()
-    assert expected == result
-  end
-
-  test "Expand integer division" do
-    result =
-      DecimalEnv.decimal do
-        div(42.1, 2.0)
-      end
-    expected = Decimal.div_int(Decimal.new(42.1), Decimal.new(2.0))
-    assert expected == result
-  end
-
-  test "Expand reduce" do
-    result =
-      DecimalEnv.decimal do
-        reduce(100.0)
-      end
-    expected = 100.0 |> Decimal.new() |> Decimal.reduce()
-    assert expected == result
-  end
-
-  test "Expand rem" do
-    result =
-      DecimalEnv.decimal do
-        rem(42.1, 2.0)
-      end
-    expected = Decimal.rem(Decimal.new(42.1), Decimal.new(2.0))
-    assert expected == result
-  end
-
-  test "Expand round" do
-    result =
-      DecimalEnv.decimal do
-        round(42.9)
-      end
-    expected = 42.9 |> Decimal.new() |> Decimal.round()
-    assert expected == result
-  end
-
-  test "Expand tuple > 3 members" do
-    result =
-      DecimalEnv.decimal do
-        {1, 2, 3}
-      end
-    expected = {Decimal.new(1), Decimal.new(2), Decimal.new(3)}
-    assert expected == result
-  end
-
-  test "Expand list" do
-    result =
-      DecimalEnv.decimal do
-        [1, 2, 3]
-      end
-    expected = [1, 2, 3] |> Enum.map(&Decimal.new/1)
-    assert expected == result
-  end
-
-  test "Expand tuple < 3 members" do
-    result =
-      DecimalEnv.decimal do
-        {1, 2}
-      end
-    expected = {Decimal.new(1), Decimal.new(2)}
-    assert expected == result
-  end
-
-  test "Expand if statement" do
-    result =
-      DecimalEnv.decimal do
-        if 42.0 > 41.0, do: {:ok, 42.0}, else: {:error, 41.0}
-      end
-    assert {:ok, Decimal.new(42.0)} == result
-  end
-
-  test "Expand binary and" do
-    result =
-      DecimalEnv.decimal do
-        42.0 < 41.0 and true
-      end
-    assert result == false
-  end
-
-  test "Expand binary or" do
-    result =
-      DecimalEnv.decimal do
-        42.0 < 41.0 or true
-      end
-    assert result == true
-  end
-
-  test "Expand binary at runtime" do
-    number = "42.0"
-    result =
-      DecimalEnv.decimal bind: [number: number] do
-        number
-      end
-    expected = Decimal.new("42.0")
-    assert expected == result
-  end
-
-  test "Expand number at runtime" do
-    number = 42.0
-    result =
-      DecimalEnv.decimal bind: [number: number] do
-        number
-      end
-    expected = Decimal.new(number)
-    assert expected == result
-  end
-
-  test "Expand tuple at runtime" do
-    tuple = {1, 2, 3}
-    result =
-      DecimalEnv.decimal bind: [tuple: tuple] do
-        tuple
-      end
-    expected = {Decimal.new(1), Decimal.new(2), Decimal.new(3)}
-    assert expected == result
-  end
-
-  test "Expand list at runtime" do
-    list = [1, 2, 3]
-    result =
-      DecimalEnv.decimal bind: [list: list] do
-        list
-      end
-    expected = Enum.map(list, &Decimal.new/1)
-    assert expected == result
-  end
-
-  test "Expand atom at runtime" do
-    atom = :atom
-    result =
-      DecimalEnv.decimal do
-        atom
-      end
-    assert atom == result
-  end
-
-  test "Expand Decimal at runtime" do
-    decimal = Decimal.new(42.0)
-    result =
-      DecimalEnv.decimal do
-        decimal
-      end
-    assert decimal == result
-  end
-
-  test "Decimal with context" do
-    result =
-      DecimalEnv.decimal context: [precision: 1] do
-        42.0
-      end
-    expected =
-      Decimal.with_context(
-        %Decimal.Context{Decimal.get_context() | precision: 1},
-        fn -> Decimal.new(42.0) end
-      )
-    assert expected == result
+      assert "42" = result
+    end
   end
 end
